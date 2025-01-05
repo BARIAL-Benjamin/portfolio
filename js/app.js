91b0c2fe4f3461121eec50bb24658a0c874f9847
Benjamin <benjamin.barial@etudiant.univ-reims.fr>
2025-01-05 18:23:14 +0100

    Compétences

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/** 
+ * @module Skills 
+ * @requires /lib/skills.mjs 
+ */
+import Skills from '/lib/skills.mjs';
+
 const age = (() => {
     const b = new Date("2002-04-15");
     const n = new Date();
@@ -12,3 +18,37 @@ const ages = document.querySelectorAll('.age');
 ages.forEach(el => {
     el.textContent = `${age} ans`;
 });
+
+if (location.href.includes('competences')) {
+    /** Formulaire des compétences
+     * @type {HTMLFormElement} 
+     */
+    const form = document.getElementById('skillsForm');
+    /** Input du nom de la compétence
+     * @type {HTMLInputElement} 
+     */
+    const name = document.getElementById('name');
+    /** Input de la description de la compétence
+     * @type {HTMLInputElement} 
+     */
+    const desc = document.getElementById('description');
+    /** Input du niveau de la compétence
+     * @type {HTMLSelectElement} 
+     */
+    const level = document.getElementById('level');
+    /** Message d'erreur
+     * @type {HTMLParagraphElement} 
+     */
+    const error = document.getElementById('error');
+
+    /** Instance de Skills pour gérer les compétences
+     * @type {Skills}
+     */
+    const skills = new Skills();
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        skills.addSkill(name.value, level.value, desc.value);
+        form.reset();
+    });
+};
